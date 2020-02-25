7cf0aad0ba5c51affb6d82cc16905635ad4c6dfd
ismo.torvinen@valmetpartners.com <ismo.torvinen@valmetpartners.com>
2020-02-25 09:15:03 +0200

    added missing style prop

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import * as plotly from 'plotly.js';
-import * as React from 'react';
+import * as plotly from "plotly.js";
+import * as React from "react";
 
 export interface IPlotlyChartProps {
   config?: any;
   data: any;
   layout?: any;
+  style?: any;
   onAfterExport?: () => void;
   onAfterPlot?: () => void;
   onAnimated?: () => void;
@@ -48,25 +49,28 @@ class PlotlyChart extends React.Component<IPlotlyChartProps, any> {
 
   public attachListeners() {
     if (this.props.onAfterExport) {
-      this.container!.on('plotly_afterexport', this.props.onAfterExport);
+      this.container!.on("plotly_afterexport", this.props.onAfterExport);
     }
     if (this.props.onAfterPlot) {
-      this.container!.on('plotly_afterplot', this.props.onAfterPlot);
+      this.container!.on("plotly_afterplot", this.props.onAfterPlot);
     }
     if (this.props.onAnimated) {
-      this.container!.on('plotly_animated', this.props.onAnimated);
+      this.container!.on("plotly_animated", this.props.onAnimated);
     }
     if (this.props.onAnimatingFrame) {
-      this.container!.on('plotly_animatingframe', this.props.onAnimatingFrame);
+      this.container!.on("plotly_animatingframe", this.props.onAnimatingFrame);
     }
     if (this.props.onAnimationInterrupted) {
-      this.container!.on('plotly_animationinterrupted', this.props.onAnimationInterrupted);
+      this.container!.on(
+        "plotly_animationinterrupted",
+        this.props.onAnimationInterrupted
+      );
     }
     if (this.props.onAutoSize) {
-      this.container!.on('plotly_autosize', this.props.onAutoSize);
+      this.container!.on("plotly_autosize", this.props.onAutoSize);
     }
     if (this.props.onBeforeExport) {
-      this.container!.on('plotly_beforeexport', this.props.onBeforeExport);
+      this.container!.on("plotly_beforeexport", this.props.onBeforeExport);
     }
     // did not find onButtonClicked in @types/plotly.js?
     // if (this.props.onButtonClicked) {
@@ -74,81 +78,90 @@ class PlotlyChart extends React.Component<IPlotlyChartProps, any> {
     // }
     if (this.props.onClick) {
       this.container!.removeAllListeners("plotly_click");
-      this.container!.on('plotly_click', this.props.onClick);
+      this.container!.on("plotly_click", this.props.onClick);
     }
     if (this.props.onClickAnnotation) {
       this.container!.removeAllListeners("plotly_clickannotation");
-      this.container!.on('plotly_clickannotation', this.props.onClickAnnotation);
+      this.container!.on(
+        "plotly_clickannotation",
+        this.props.onClickAnnotation
+      );
     }
     if (this.props.onDeselect) {
-        this.container!.removeAllListeners("plotly_deselect");
-      this.container!.on('plotly_deselect', this.props.onDeselect);
+      this.container!.removeAllListeners("plotly_deselect");
+      this.container!.on("plotly_deselect", this.props.onDeselect);
     }
     if (this.props.onDoubleClick) {
-        this.container!.removeAllListeners("plotly_doubleclick");
-      this.container!.on('plotly_doubleclick', this.props.onDoubleClick);
+      this.container!.removeAllListeners("plotly_doubleclick");
+      this.container!.on("plotly_doubleclick", this.props.onDoubleClick);
     }
     if (this.props.onFramework) {
-        this.container!.removeAllListeners("plotly_framework");
-      this.container!.on('plotly_framework', this.props.onFramework);
+      this.container!.removeAllListeners("plotly_framework");
+      this.container!.on("plotly_framework", this.props.onFramework);
     }
     if (this.props.onHover) {
-        this.container!.removeAllListeners("plotly_hover");
-      this.container!.on('plotly_hover', this.props.onHover);
+      this.container!.removeAllListeners("plotly_hover");
+      this.container!.on("plotly_hover", this.props.onHover);
     }
     if (this.props.onLegendClick) {
-        this.container!.removeAllListeners("plotly_legendclick");
-      this.container!.on('plotly_legendclick', this.props.onLegendClick);
+      this.container!.removeAllListeners("plotly_legendclick");
+      this.container!.on("plotly_legendclick", this.props.onLegendClick);
     }
     if (this.props.onLegendDoubleClick) {
-        this.container!.removeAllListeners("plotly_legenddoubleclick");
-      this.container!.on('plotly_legenddoubleclick', this.props.onLegendDoubleClick);
+      this.container!.removeAllListeners("plotly_legenddoubleclick");
+      this.container!.on(
+        "plotly_legenddoubleclick",
+        this.props.onLegendDoubleClick
+      );
     }
     if (this.props.onRelayout) {
-        this.container!.removeAllListeners("plotly_relayout");
-      this.container!.on('plotly_relayout', this.props.onRelayout);
+      this.container!.removeAllListeners("plotly_relayout");
+      this.container!.on("plotly_relayout", this.props.onRelayout);
     }
     if (this.props.onRestyle) {
-        this.container!.removeAllListeners("plotly_restyle");
-      this.container!.on('plotly_restyle', this.props.onRestyle);
+      this.container!.removeAllListeners("plotly_restyle");
+      this.container!.on("plotly_restyle", this.props.onRestyle);
     }
     if (this.props.onRedraw) {
-        this.container!.removeAllListeners("plotly_redraw");
-      this.container!.on('plotly_redraw', this.props.onRedraw);
+      this.container!.removeAllListeners("plotly_redraw");
+      this.container!.on("plotly_redraw", this.props.onRedraw);
     }
     if (this.props.onSelecting) {
-        this.container!.removeAllListeners("plotly_selecting");
-      this.container!.on('plotly_selecting', this.props.onSelecting);
+      this.container!.removeAllListeners("plotly_selecting");
+      this.container!.on("plotly_selecting", this.props.onSelecting);
     }
     if (this.props.onSliderChange) {
-        this.container!.removeAllListeners("plotly_sliderchange");
-      this.container!.on('plotly_sliderchange', this.props.onSliderChange);
+      this.container!.removeAllListeners("plotly_sliderchange");
+      this.container!.on("plotly_sliderchange", this.props.onSliderChange);
     }
     if (this.props.onSliderEnd) {
-        this.container!.removeAllListeners("plotly_sliderend");
-      this.container!.on('plotly_sliderend', this.props.onSliderEnd);
+      this.container!.removeAllListeners("plotly_sliderend");
+      this.container!.on("plotly_sliderend", this.props.onSliderEnd);
     }
     if (this.props.onSliderStart) {
-        this.container!.removeAllListeners("plotly_sliderstart");
-      this.container!.on('plotly_sliderstart', this.props.onSliderStart);
+      this.container!.removeAllListeners("plotly_sliderstart");
+      this.container!.on("plotly_sliderstart", this.props.onSliderStart);
     }
     if (this.props.onTransitioning) {
-        this.container!.removeAllListeners("plotly_transitioning");
-      this.container!.on('plotly_transitioning', this.props.onTransitioning);
+      this.container!.removeAllListeners("plotly_transitioning");
+      this.container!.on("plotly_transitioning", this.props.onTransitioning);
     }
     if (this.props.onTransitionInterrupted) {
-        this.container!.removeAllListeners("plotly_transitioninterrupted");
-      this.container!.on('plotly_transitioninterrupted', this.props.onTransitionInterrupted);
+      this.container!.removeAllListeners("plotly_transitioninterrupted");
+      this.container!.on(
+        "plotly_transitioninterrupted",
+        this.props.onTransitionInterrupted
+      );
     }
     if (this.props.onUnHover) {
-        this.container!.removeAllListeners("plotly_unhover");
-      this.container!.on('plotly_unhover', this.props.onUnHover);
+      this.container!.removeAllListeners("plotly_unhover");
+      this.container!.on("plotly_unhover", this.props.onUnHover);
     }
     if (this.props.onEvent) {
-        this.container!.removeAllListeners("plotly_event");
-      this.container!.on('plotly_event', this.props.onEvent);
+      this.container!.removeAllListeners("plotly_event");
+      this.container!.on("plotly_event", this.props.onEvent);
     }
-    window.addEventListener('resize', this.resize);
+    window.addEventListener("resize", this.resize);
   }
 
   public resize = () => {
@@ -161,7 +174,12 @@ class PlotlyChart extends React.Component<IPlotlyChartProps, any> {
     const { data, layout, config } = props;
     if (this.container) {
       // plotly.react will not destroy the old plot: https://plot.ly/javascript/plotlyjs-function-reference/#plotlyreact
-      this.container = await plotly.react(this.container, data, Object.assign({}, layout), config);
+      this.container = await plotly.react(
+        this.container,
+        data,
+        Object.assign({}, layout),
+        config
+      );
       this.attachListeners();
     }
   };
@@ -178,14 +196,14 @@ class PlotlyChart extends React.Component<IPlotlyChartProps, any> {
     if (this.container) {
       plotly.purge(this.container);
     }
-    window.removeEventListener('resize', this.resize);
+    window.removeEventListener("resize", this.resize);
   }
 
   public render() {
-    const { 
-      data, 
-      layout, 
-      config, 
+    const {
+      data,
+      layout,
+      config,
       onAfterExport,
       onAfterPlot,
       onAnimated,
@@ -193,34 +211,39 @@ class PlotlyChart extends React.Component<IPlotlyChartProps, any> {
       onAnimationInterrupted,
       onAutoSize,
       onBeforeExport,
-      onClick, 
+      onClick,
       onClickAnnotation,
       onDeselect,
       onDoubleClick,
       onFramework,
-      onHover, 
+      onHover,
       onLegendClick,
       onLegendDoubleClick,
       onRelayout,
       onRestyle,
       onRedraw,
-      onSelected, 
+      onSelected,
       onSelecting,
       onSliderChange,
       onSliderEnd,
       onSliderStart,
       onTransitioning,
       onTransitionInterrupted,
-      onUnHover, 
+      onUnHover,
       onEvent,
-      ...other 
+      ...other
     } = this.props;
     return (
       <div
         {...other}
         ref={async node => {
           if (node && !this.container) {
-            this.container = await plotly.newPlot(node, data as any, Object.assign({}, layout), config);
+            this.container = await plotly.newPlot(
+              node,
+              data as any,
+              Object.assign({}, layout),
+              config
+            );
             this.attachListeners();
           }
         }}
